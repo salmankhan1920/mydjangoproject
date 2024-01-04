f82ad795618a93ad4b0ab919d54ca63a4b12a986
salman khan <salmankhan1920@protonmail.com>
2024-01-04 17:59:36 +0530

    made changes to settings.py file and added vercel configuration

--- myproject/settings.py
+++ myproject/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 import os
+import dj_database_url
+
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -24,12 +26,19 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('SECRET_KEY')
+
+SECRET_KEY = os.environ.get('SECRET_KEY', "django-insecure-hh_5bli3gh9cxbl2j&0!=t*=h2$89m@2g@+&bfaj=xk)p&sf_j")
+
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEBUG', 'True') == 'True'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '127.0.0.1').split(",")
+
+SUPABASE_DB_URL = os.environ.get("SUPABASE_DB_URL")
+
+DJANGO_ENV = os.environ.get('DJANGO_ENV')
 
 
 # Application definition
@@ -86,6 +95,27 @@ ASGI_APPLICATION = "myproject.asgi.application"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 
+
+# Check if the Supabase database URL is set
+if SUPABASE_DB_URL:
+    # Use the Supabase database configuration
+    DATABASES = {
+        "default": dj_database_url.config(
+            default=SUPABASE_DB_URL, conn_max_age=600
+        )
+    }
+else:
+    # Use the SQLite database configuration for development
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
+
+
+
+'''
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
@@ -97,7 +127,7 @@ DATABASES = {
     }
 }
 
-
+'''
 
 
 '''
@@ -161,11 +191,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = "static/"
-STATIC_ROOT = os.path.join(BASE_DIR, "static")
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "myapp", "static"),
 ]
 
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
@@ -195,4 +226,3 @@ CHANNEL_LAYERS = {
 
 
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
--- myproject/wsgi.py
+++ myproject/wsgi.py
@@ -13,4 +13,8 @@ from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "myproject.settings")
 
-application = get_wsgi_application()
+if os.environ.get('DJANGO_ENV') == 'production':
+    app = get_wsgi_application()
+else:
+    application = get_wsgi_application()
+
